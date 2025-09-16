7469c4aaf0d35ffb93204d4d3f64c1b1401af690
Lục Anh <120559330+luctananh@users.noreply.github.com>
2025-09-16 17:12:51 +0700

    cập nhật

--- bookstoree/Models/DiscountCode.cs
+++ bookstoree/Models/DiscountCode.cs
@@ -16,13 +16,17 @@ namespace bookstoree.Models
         
         [DisplayName("Giá trị")]
         // Value of the discount. If DiscountType is "Percent", this is a percentage. If "Fixed", this is a currency amount.
+        [Required(ErrorMessage = "Trường Giá trị là bắt buộc.")]
         public int Value { get; set; } 
         
         [DisplayName("Đơn hàng tối thiểu (VNĐ)")]
+        [Required(ErrorMessage = "Trường Đơn hàng tối thiểu (VNĐ) là bắt buộc.")]
         public int MinimumOrder { get; set; } // Minimum order amount to apply the discount
         [DisplayName("Ngày bắt đầu")]
+        [Required(ErrorMessage = "Trường Ngày bắt đầu là bắt buộc.")]
         public DateTime StartDate { get; set; } // Start date of the discount code validity
         [DisplayName("Ngày kết thúc")]
+        [Required(ErrorMessage = "Trường Ngày kết thúc là bắt buộc.")]
         public DateTime EndDate { get; set; } // End date of the discount code validity
         public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 
